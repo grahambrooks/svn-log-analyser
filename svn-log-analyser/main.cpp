31f3ab87a05cfa3ee9e63c40983ec624132db209
Graham Brooks <graham@grahambrooks.com>
2011-06-29 20:22:27 -0500

    Initial Commit

---
+++ svn-log-analyser/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  svn-log-analyser
+//
+//  Created by Graham Brooks on 29/06/2011.
+//  Copyright 2011 ThoughtWorks. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
